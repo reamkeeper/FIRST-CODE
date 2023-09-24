d0e568c6b7415cb06482803ba8b5abab038f9c6b
reamkeeper <142344784+reamkeeper@users.noreply.github.com>
2023-09-24 11:26:06 +0800

    Update 作业7.cpp

--- 作业7.cpp
+++ 作业7.cpp
@@ -1,20 +1,16 @@
 #include<iostream>
 using namespace std;
-
 void show(int,int);
 int main(){
-		int hour,minutes;
+	int hour,minutes;
 	cin>>hour;
 	cin>>minutes;
-	
 	show(hour,minutes);
 	return 0;
 }
-
 void show(int h,int m ){
 	cout<<"Enter the number of hours:"<< h<<endl;
 	cout<<"Enter the number of minutes:"<< m<<endl;
 	cout<<"Time:"<<h<<":"<<m<<endl; 
-	return ;
-	
+	return ;	
 }
