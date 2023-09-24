f8c198375b64adcb5e83d59ab5c37d7f6528561a
reamkeeper <142344784+reamkeeper@users.noreply.github.com>
2023-09-24 11:25:32 +0800

    Add files via upload

---
+++ 作业7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void show(int,int);
+int main(){
+		int hour,minutes;
+	cin>>hour;
+	cin>>minutes;
+	
+	show(hour,minutes);
+	return 0;
+}
+
+void show(int h,int m ){
+	cout<<"Enter the number of hours:"<< h<<endl;
+	cout<<"Enter the number of minutes:"<< m<<endl;
+	cout<<"Time:"<<h<<":"<<m<<endl; 
+	return ;
+	
+}
